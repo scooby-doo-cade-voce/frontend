64248fc0e3f362258e993f0559a85b0379e9df0b
Matheus Grossi <thebravematteo33@protonmail.com>
2024-05-17 11:18:03 +0200

    feat/adding-tailwind+ts-to-the-project

--- cade-voce/src/App.js
+++ cade-voce/src/App.js
@@ -1,4 +1,4 @@
-import logo from './scoobydoo.svg';
+import logo from './assets/scoobydoo.svg';
 import './App.css';
 
 function App() {
---
+++ cade-voce/src/components/Header/Header.ts
@@ -0,0 +1,4 @@
+export function Header() {
+  return
+
+}   
