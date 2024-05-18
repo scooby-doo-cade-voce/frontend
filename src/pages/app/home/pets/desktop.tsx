0decf13fab89945a1adc6505ee9d0305c33fa523
Gabriel Teles <gabrielteles0609@gmail.com>
2024-05-18 04:55:54 -0300

    fix(husky&pet-card): Pet-card está se transformando em um componente dinâmico, em breve pegará todos os dados através  de um get

--- src/pages/app/home/pets/desktop.tsx
+++ src/pages/app/home/pets/desktop.tsx
@@ -1,12 +1,17 @@
+import React from 'react'
 import { PetCard } from '@/components/pet-card'
 
 export default function PetsDesktop() {
   return (
     <div className="grid gap-5 lg:grid-cols-4">
-      <PetCard />
-      <PetCard />
-      <PetCard />
-      <PetCard />
+      <PetCard
+        imageSrc="https://placedog.net/400?random"
+        title="Vira lata marrom"
+        description="Vira lata marrom com focinho preto, coleira bege, mancha branca no peito e mancha bege na pata direita de trás."
+        species="Cachorro"
+        breed="Vira lata"
+        color="Marrom"
+      />
     </div>
   )
 }
