4a896ecfc33a1fe73a4e1a9ca49de445d38c5f1c
guilherme.nascimento <gnlnascimento@gmail.com>
2024-05-18 17:38:54 -0300

    Creating subscription page done for pet subscriptions (mobile included)

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import { NotFound } from './pages/404'
 import { AppLayout } from './pages/_layouts/app'
 import { HomePage } from './pages/app/home'
+import { PetSubscriptionDonePage } from './pages/app/cadastro-concluido'
 
 export const router = createBrowserRouter([
   {
@@ -17,6 +18,10 @@ export const router = createBrowserRouter([
       {
         path: '/cadastrar-pet',
         element: <HomePage />,
+      },
+      {
+        path: '/cadastro-pet-concluido',
+        element: <PetSubscriptionDonePage />,
       },
     ],
   },
